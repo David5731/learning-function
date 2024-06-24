359df4ea8e52a42e9c0376b83eed7d09882a2645
David akinsanya <davidakinsanya69@gmail.com>
2024-06-24 15:22:37 +0100

    functions

---
+++ samples.py
@@ -0,0 +1,12 @@
+class Currencies():
+    def __init__(currency, GDP, USD, AUD, NGN):
+        currency.GDP = GDP
+        currency.USD = USD
+        currency.AUD = AUD
+        currency.NGN = NGN
+    def money(currency):
+        print(currency.GDP)
+    p1 = ( "pounds", "dollars", "australian dollars", "naira")
+p1 = (Currencies.GDP)
+
+
